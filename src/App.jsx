123019e4ee44870eea46e6159f9e8ae86112a249
Chikaturin <ninhhoaithanh@gmail.com>
2024-07-28 16:15:19 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from 'Header_Footer/Header'
-import Footer from 'Header_Footer/Footer'
+import React from "react";
+import { LoginForm } from "remote/Remote";
+import { MantineProvider } from "@mantine/hooks";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <Header/>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>MicroFE2</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Footer/>
-    </>
-  )
+    <MantineProvider>
+      <LoginForm />
+      <p>Welcome to the Micro Frontend App</p>
+    </MantineProvider>
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation'
- 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+
 export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: "microFE2",
+      name: "host-app",
       remotes: {
-        Header_Footer:"http://localhost:5001/assets/remoteEntry.js",
+        remote: "remote@https://htilssu.com/assets/remoteEntry.js",
       },
-      exposes: {
+      shared: {
+        react: {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "^18.2.0",
+        },
+        "react-dom": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "^18.2.0",
+        },
+        "@mantine/core": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "^7.11.2",
+        },
+        "@mantine/form": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "^7.11.2",
+        },
+        "@mantine/hooks": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "^7.11.2",
+        },
       },
-      shared: ["react", "react-dom"],
     }),
   ],
   build: {
@@ -21,5 +45,7 @@ export default defineConfig({
     minify: false,
     cssCodeSplit: false,
   },
-  
-})
+  optimizeDeps: {
+    include: ["@mantine/core", "@mantine/hooks", "@mantine/form"],
+  },
+});
